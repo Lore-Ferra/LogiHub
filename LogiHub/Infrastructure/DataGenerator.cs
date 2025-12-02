b7589bbdd7017d24544f54568cd2dd0c4bbf3dbc
Nick-2002b <nicolae.balaban@studio.unibo.it>
2025-12-03 00:16:23 +0100

    feat: popolato tabelle nuove con dati a runtime

--- LogiHub/Infrastructure/DataGenerator.cs
+++ LogiHub/Infrastructure/DataGenerator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Linq.Dynamic.Core;
 using LogiHub.Services;
 using LogiHub.Services.Shared;
 
@@ -9,41 +10,133 @@ namespace LogiHub.Infrastructure
     {
         public static void InitializeUsers(TemplateDbContext context)
         {
-            if (context.Users.Any())
+            
+            var idAzione1 = Guid.NewGuid();
+            var idAzione2 = Guid.NewGuid();
+
+            var idAzienda1 = Guid.NewGuid();
+            var idAzienda2 = Guid.NewGuid();
+
+            var idSemiLavorato1 = Guid.NewGuid();
+            var idSemiLavorato2 = Guid.NewGuid();
+            var idSemiLavorato3 = Guid.NewGuid();
+            if (!context.Users.Any())
             {
-                return;   // Data was already seeded
+                context.Users.AddRange(
+                    new User
+                    {
+                        Id = Guid.Parse("3de6883f-9a0b-4667-aa53-0fbc52c4d300"), // Forced to specific Guid for tests
+                        Email = "email1@test.it",
+                        Password = "M0Cuk9OsrcS/rTLGf5SY6DUPqU2rGc1wwV2IL88GVGo=", // SHA-256 of text "Prova"
+                        FirstName = "Nome1",
+                        LastName = "Cognome1",
+                        NickName = "Nickname1"
+                    },
+                    new User
+                    {
+                        Id = Guid.Parse("a030ee81-31c7-47d0-9309-408cb5ac0ac7"), // Forced to specific Guid for tests
+                        Email = "email2@test.it",
+                        Password = "Uy6qvZV0iA2/drm4zACDLCCm7BE9aCKZVQ16bg80XiU=", // SHA-256 of text "Test"
+                        FirstName = "Nome2",
+                        LastName = "Cognome2",
+                        NickName = "Nickname2"
+                    },
+                    new User
+                    {
+                        Id = Guid.Parse("bfdef48b-c7ea-4227-8333-c635af267354"), // Forced to specific Guid for tests
+                        Email = "email3@test.it",
+                        Password = "Uy6qvZV0iA2/drm4zACDLCCm7BE9aCKZVQ16bg80XiU=", // SHA-256 of text "Test"
+                        FirstName = "Nome3",
+                        LastName = "Cognome3",
+                        NickName = "Nickname3"
+                    });
             }
 
-            context.Users.AddRange(
-                new User
+            if (!context.Users.Any())
+            {
+                var righe = new[] { "A", "B", "C" };
+                var colonne = 5;
+                foreach (var riga in righe)
                 {
-                    Id = Guid.Parse("3de6883f-9a0b-4667-aa53-0fbc52c4d300"), // Forced to specific Guid for tests
-                    Email = "email1@test.it",
-                    Password = "M0Cuk9OsrcS/rTLGf5SY6DUPqU2rGc1wwV2IL88GVGo=", // SHA-256 of text "Prova"
-                    FirstName = "Nome1",
-                    LastName = "Cognome1",
-                    NickName = "Nickname1"
-                },
-                new User
-                {
-                    Id = Guid.Parse("a030ee81-31c7-47d0-9309-408cb5ac0ac7"), // Forced to specific Guid for tests
-                    Email = "email2@test.it",
-                    Password = "Uy6qvZV0iA2/drm4zACDLCCm7BE9aCKZVQ16bg80XiU=", // SHA-256 of text "Test"
-                    FirstName = "Nome2",
-                    LastName = "Cognome2",
-                    NickName = "Nickname2"
-                },
-                new User
-                {
-                    Id = Guid.Parse("bfdef48b-c7ea-4227-8333-c635af267354"), // Forced to specific Guid for tests
-                    Email = "email3@test.it",
-                    Password = "Uy6qvZV0iA2/drm4zACDLCCm7BE9aCKZVQ16bg80XiU=", // SHA-256 of text "Test"
-                    FirstName = "Nome3",
-                    LastName = "Cognome3",
-                    NickName = "Nickname3"
-                });
+                    for (int i = 1; i <= colonne; i++)
+                    {
+                        context.Ubicazioni.Add(new Ubicazione
+                        {
+                            UbicazioneId = Guid.NewGuid(),
+                            Posizione = $"{riga}{i}"
+                        });
+                    }
+                }
+            }
+
+            if (!context.AziendeEsterne.Any())
+            {
+                context.AziendeEsterne.AddRange(
+                    new AziendaEsterna { Id = idAzienda1, Nome = "Azienda Esterna 1", Indirizzo = "Indirizzo 1" },
+                    new AziendaEsterna { Id = idAzienda2, Nome = "Azienda Esterna 2", Indirizzo = "Indirizzo 2" }
+                    );
+            }
 
             context.SaveChanges();
+
+            if (!context.SemiLavorati.Any())
+            {
+                context.SemiLavorati.AddRange(
+                    new SemiLavorato
+                    {
+                        Id = idSemiLavorato1,
+                        AziendaEsternaId = idAzienda1,
+                        Descrizione = "Sbarra di metallo",
+                        DataCreazione = DateTime.Now,
+                        UltimaModifica = DateTime.Now.AddDays(-2),
+                        Ubicazione = context.Ubicazioni.FirstOrDefault(x => x.Posizione == "A1")
+                    },
+                    new SemiLavorato
+                    {
+                        Id = idSemiLavorato2,
+                        AziendaEsternaId = idAzienda2,
+                        Descrizione = "Trave",
+                        DataCreazione = DateTime.Now,
+                        UltimaModifica = DateTime.Now.AddDays(-3),
+                        Ubicazione = context.Ubicazioni.FirstOrDefault(x => x.Posizione == "B1")
+                    },
+                    new SemiLavorato
+                    {
+                        Id = idSemiLavorato3,
+                        AziendaEsternaId = idAzienda1,
+                        Descrizione = "Lastra",
+                        DataCreazione = DateTime.Now,
+                        UltimaModifica = DateTime.Now.AddDays(-4),
+                        Ubicazione = context.Ubicazioni.FirstOrDefault(x => x.Posizione == "C1")
+                    }
+                    );
+            }
+
+            context.SaveChanges();
+
+            if (!context.Azioni.Any())
+            { 
+                context.Azioni.AddRange(
+                    new Azione
+                    {
+                        Id = idAzione1,
+                        Dettagli = "Arrivo Merce da fornitore",
+                        SemiLavoratoId = idSemiLavorato1,
+                        TipoOperazione = "Spostamento",
+                        UserId = Guid.Parse("3de6883f-9a0b-4667-aa53-0fbc52c4d300"),
+                        DataOperazione = DateTime.Now.AddDays(-10)
+                    },
+                    new Azione
+                    {
+                    Id = idAzione2,
+                        Dettagli = "Spostato in verniciatura",
+                        SemiLavoratoId = idSemiLavorato1,
+                        TipoOperazione = "Spostamento",
+                        UserId = Guid.Parse("3de6883f-9a0b-4667-aa53-0fbc52c4d300"),
+                        DataOperazione = DateTime.Now.AddDays(-5),
+                    });
+                context.SaveChanges();
+            }
         }
     }
 }
