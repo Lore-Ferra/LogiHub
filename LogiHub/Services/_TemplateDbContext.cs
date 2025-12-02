813f4b17c44470a6e0dbd5186b9ad24ecc4e8e5d
Nick-2002b <nicolae.balaban@studio.unibo.it>
2025-12-02 20:20:21 +0100

    feat: registrazione delle nuove tabelle nel dbcontext

--- LogiHub/Services/_TemplateDbContext.cs
+++ LogiHub/Services/_TemplateDbContext.cs
@@ -16,5 +16,9 @@ namespace LogiHub.Services
         }
 
         public DbSet<User> Users { get; set; }
+        public DbSet<AziendaEsterna> AziendeEsterne { get; set; }
+        public DbSet<SemiLavorato> SemiLavorati { get; set; }
+        public DbSet<Ubicazione> Ubicazioni { get; set; }
+        public DbSet<Azione> Azioni { get; set; }
     }
 }
