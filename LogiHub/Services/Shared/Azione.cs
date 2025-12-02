6813c72e0a635c8e1b8d8b47cb264a934b34b462
Nick-2002b <nicolae.balaban@studio.unibo.it>
2025-12-02 20:46:46 +0100

    fix: cambiato SemiLavoratoId in Guid type per il giusto funzionamento con EF

--- LogiHub/Services/Shared/Azione.cs
+++ LogiHub/Services/Shared/Azione.cs
@@ -8,7 +8,7 @@ public class Azione
     [Key]
     public Guid Id { get; set; }
 
-    public int? SemiLavoratoId { get; set; }
+    public Guid? SemiLavoratoId { get; set; }
 
     [ForeignKey(nameof(SemiLavoratoId))]
     public SemiLavorato SemiLavorato { get; set; }
