fc761f2fcbeb0f4416ca13d0ed6d0f40880167b4
Nick-2002b <nicolae.balaban@studio.unibo.it>
2025-12-02 23:43:36 +0100

    feat: aggiunto nuova property Id in ubicazione

--- LogiHub/Services/Shared/Ubicazione.cs
+++ LogiHub/Services/Shared/Ubicazione.cs
@@ -1,4 +1,9 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
 public class Ubicazione
 {
+    [Key]
+    public Guid? UbicazioneId { get; set; }
     public string Posizione { get; set; } = string.Empty;
 }
