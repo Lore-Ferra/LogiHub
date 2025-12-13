f81f794a304867b69ec3b8b578e6932d21a50d72
Lore-Ferra <lorenzo.ferrari27@studio.unibo.it>
2025-12-13 15:44:55 +0100

    aggiunta classe inventario

---
+++ LogiHub/Models/Shared/Inventario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using LogiHub.Models.Shared;
+
+public class Inventario
+{
+    [Key]
+    public Guid Id { get; set; }
+
+    public Guid UbicazioneId { get; set; }
+    [ForeignKey(nameof(UbicazioneId))]
+    public Ubicazione Ubicazione { get; set; }
+
+    public string SemiLavoratoId { get; set; }
+    [ForeignKey(nameof(SemiLavoratoId))]
+    public SemiLavorato SemiLavorato { get; set; }
+    public bool Presente { get; set; }
+
+    public DateTime DataInventario { get; set; } = DateTime.Now;
+
+    public Guid UserId { get; set; }
+    [ForeignKey(nameof(UserId))]
+    public User User { get; set; }
+
+    public string? Note { get; set; }
+}
