f3f6ffada42bfe373a30372d7dffae0e5b22a66f
Nick-2002b <nicolae.balaban@studio.unibo.it>
2025-12-14 17:07:15 +0100

    feat: implementazione del viewmodel per index SemiLavorati

---
+++ LogiHub.Web1/Areas/Magazzino/IndexViewModel.cs
@@ -0,0 +1,23 @@
+﻿using LogiHub.Services.Shared;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace LogiHub.Web.Areas.Magazzino
+{
+    public class IndexViewModel
+    {
+        public IEnumerable<SemiLavoratiIndexDTO.RigaSemiLavorato> SemiLavorati { get; set; } = new List<SemiLavoratiIndexDTO.RigaSemiLavorato>();
+        public string Filter { get; set; }
+        public void SetData(SemiLavoratiIndexDTO dto)
+        {
+            SemiLavorati = dto.Items;
+        }
+        public SemilavoratiIndexQuery ToQuery()
+        {
+            return new SemilavoratiIndexQuery
+            {
+                Filter = this.Filter
+            };
+        }
+    }
+}
