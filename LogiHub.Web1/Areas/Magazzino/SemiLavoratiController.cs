d90aec40de34bef9262b3f066ce782adf64aa1a5
Nick-2002b <nicolae.balaban@studio.unibo.it>
2025-12-14 17:17:54 +0100

    feat: implementazione SemilavoratiController

--- LogiHub.Web1/Areas/Magazzino/SemiLavoratiController.cs
+++ LogiHub.Web1/Areas/Magazzino/SemiLavoratiController.cs
@@ -1,6 +1,27 @@
-﻿namespace LogiHub.Web.Areas.Magazzino.Controllers
+﻿using LogiHub.Services.Shared;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace LogiHub.Web.Areas.Magazzino
 {
-    public class SemiLavoratiController
+    [Area("Magazzino")]
+    public partial class SemiLavoratiController : Controller
     {
+        private readonly SharedService _queries;
+        private readonly ISemiLavoratoService _service;
+        public SemiLavoratiController(SharedService queries, ISemiLavoratoService service)
+        {
+            _queries = queries;
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index(SemilavoratiIndexQuery query)
+        {
+            var dto = await _queries.Query(query);
+            var model = new IndexViewModel();
+            model.SetData(dto);
+            return View(model);
+        }
     }
 }
