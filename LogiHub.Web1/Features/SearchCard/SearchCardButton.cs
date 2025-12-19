a77ea006b2f6537831d79c78f9ac3fac3504dcc5
Alex <alex.cambrini@studio.unibo.it>
2025-12-19 13:39:28 +0100

    Add: implementata SearchCard con supporto per pulsanti e campi nascosti

---
+++ LogiHub.Web1/Features/SearchCard/SearchCardButton.cs
@@ -0,0 +1,10 @@
+﻿namespace LogiHub.Web1.Features.SearchCard;
+
+public class SearchCardButton
+{
+    public string Text { get; set; }
+    public string CssClass { get; set; } = "btn-primary";
+    public string Type { get; set; } = "submit"; 
+    public string OnClick { get; set; }
+    public string IconClass { get; set; }
+}
---
+++ LogiHub.Web1/Features/SearchCard/SearchCardViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace LogiHub.Web1.Features.SearchCard;
+
+using System.Collections.Generic;
+
+public class SearchCardViewModel
+{
+    public string Title { get; set; }
+    public string InputName { get; set; } = "Filter";
+    public string InputValue { get; set; }
+    public string Placeholder { get; set; } = "Cerca…";
+    public List<SearchCardButton> Buttons { get; set; } = new();
+    public Dictionary<string, string> HiddenFields { get; set; } = new();
+}
