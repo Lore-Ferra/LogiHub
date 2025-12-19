2e338531281b7c8274085c2675db1e1bf20139ec
Alex <alex.cambrini@studio.unibo.it>
2025-12-19 19:17:49 +0100

    Refactor: aggiunto il DTO EliminaSemiLavorato per la gestione dei semi lavorati

---
+++ LogiHub/Services/Magazzino/SemiLavorati/DTO/EliminaSemiLavoratoDTO.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace LogiHub.Services.Shared.SemiLavorati;
+
+public class EliminaSemiLavoratoDTO
+{
+    public string SemiLavoratoId { get; set; }
+    public string Dettagli { get; set; }
+    public Guid UserId { get; set; }
+}
