e9160219850bdd58d68b33f29ffa79b49e8c45cd
Alex <alex.cambrini@studio.unibo.it>
2025-12-19 18:09:04 +0100

    Fix: aggiunta la gestione delle password

--- LogiHub/Infrastructure/DataGenerator.cs
+++ LogiHub/Infrastructure/DataGenerator.cs
@@ -31,9 +31,27 @@ namespace LogiHub.Infrastructure
             if (context.Users.Any()) return;
 
             context.Users.AddRange(
-                new User { Id = Guid.Parse("3de6883f-9a0b-4667-aa53-0fbc52c4d300"), Email = "email1@test.it", FirstName = "Nome1", LastName = "Cognome1" },
-                new User { Id = Guid.Parse("a030ee81-31c7-47d0-9309-408cb5ac0ac7"), Email = "email2@test.it", FirstName = "Nome2", LastName = "Cognome2" },
-                new User { Id = Guid.Parse("bfdef48b-c7ea-4227-8333-c635af267354"), Email = "email3@test.it", FirstName = "Nome3", LastName = "Cognome3" }
+                new User 
+                { 
+                    Id = Guid.Parse("3de6883f-9a0b-4667-aa53-0fbc52c4d300"), 
+                    Email = "email1@test.it", 
+                    Password = "M0Cuk9OsrcS/rTLGf5SY6DUPqU2rGc1wwV2IL88GVGo=", 
+                    FirstName = "Nome1", LastName = "Cognome1" 
+                },
+                new User 
+                { 
+                    Id = Guid.Parse("a030ee81-31c7-47d0-9309-408cb5ac0ac7"), 
+                    Email = "email2@test.it", 
+                    Password = "Uy6qvZV0iA2/drm4zACDLCCm7BE9aCKZVQ16bg80XiU=", 
+                    FirstName = "Nome2", LastName = "Cognome2" 
+                },
+                new User 
+                { 
+                    Id = Guid.Parse("bfdef48b-c7ea-4227-8333-c635af267354"), 
+                    Email = "email3@test.it", 
+                    Password = "Uy6qvZV0iA2/drm4zACDLCCm7BE9aCKZVQ16bg80XiU=", 
+                    FirstName = "Nome3", LastName = "Cognome3" 
+                }
             );
         }
 
