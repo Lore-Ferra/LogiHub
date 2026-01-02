82fd6d7dd6ca6dbc68b498085082b9eb9d4d087a
Alex <alex.cambrini@studio.unibo.it>
2026-01-02 11:49:13 +0100

    Refactor: rimosse le dichiarazioni di metodo non utilizzate nell'interfaccia ISemiLavoratoService

--- LogiHub/Services/Magazzino/SemiLavorati/ISemiLavoratoService.cs
+++ LogiHub/Services/Magazzino/SemiLavorati/ISemiLavoratoService.cs
@@ -1,45 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using LogiHub.Services.Shared.SemiLavorati;
 
 public interface ISemiLavoratoService
 {
-    
     Task<SemiLavorato> AggiungiSemiLavoratoAsync(AggiungiSemiLavoratoDTO dto);
     Task<bool> EliminaAsync(EliminaSemiLavoratoDTO dto);
     Task<bool> ModificaSemiLavorato(ModificaSemiLavoratoDTO dto);
-    //
-    //
-    //
-    // Task<SemiLavorato?> GetByIdAsync(string id);
-    //
-    // Task<bool> AggiornaAsync(
-    //     string id,
-    //     string? nuovaDescrizione,
-    //     Guid? nuovaUbicazioneId,
-    //     Guid? nuovaAziendaEsternaId,
-    //     Guid userId,
-    //     string dettagli
-    // );
-    //
-    // Task<bool> CambiaUbicazioneAsync(
-    //     string semiLavoratoId,
-    //     Guid nuovaUbicazioneId,
-    //     Guid userId,
-    //     string dettagli
-    // );
-    //
-    // Task<bool> InviaAdAziendaEsternaAsync(
-    //     string semiLavoratoId,
-    //     Guid aziendaEsternaId,
-    //     Guid userId,
-    //     string dettagli
-    // );
-    //
-    // Task<bool> RegistraRientroAsync(
-    //     string semiLavoratoId,
-    //     Guid userId,
-    //     string dettagli
-    // );
 }
