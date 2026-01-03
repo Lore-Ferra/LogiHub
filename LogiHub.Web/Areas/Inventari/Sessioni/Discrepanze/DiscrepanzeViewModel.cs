1547e5c0f71812208ae25a06bc28bfdc19d16960
Nick-2002b <nicolae.balaban@studio.unibo.it>
2026-01-04 00:34:26 +0100

    feat: implementato viewmodel per Discrepanze

--- LogiHub.Web/Areas/Inventari/Sessioni/Discrepanze/DiscrepanzeViewModel.cs
+++ LogiHub.Web/Areas/Inventari/Sessioni/Discrepanze/DiscrepanzeViewModel.cs
@@ -1,6 +1,25 @@
+using System;
+using System.Collections.Generic;
+using LogiHub.Services.Inventari.Sessioni.DTO;
+using LogiHub.Web.Features.SearchCard;
+using LogiHub.Web.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+
 namespace LogiHub.Web.Areas.Inventari.Sessioni.Discrepanze;
 
-public class DiscrepanzeViewModel
+public class DiscrepanzeViewModel : PagingViewModel
 {
-    
+    public Guid SessioneId { get; set; }
+    public SearchCardViewModel SearchCard { get; set; }
+    public IEnumerable<DiscrepanzaDTO> Discrepanze { get; set; }
+
+    public override IActionResult GetRoute()
+    {
+        return MVC.Inventari.Discrepanze.Index(
+            SessioneId,
+            SearchCard.Query,
+            Page,
+            PageSize
+        ).GetAwaiter().GetResult();
+    }
 }
