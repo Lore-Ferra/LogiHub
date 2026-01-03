b38de06645932c253ab6ec1b5e4e45a88470f4f9
Alex <alex.cambrini@studio.unibo.it>
2026-01-03 18:29:42 +0100

    feat: aggiunta vista di rilevamento ubicazione e gestione dei pezzi

---
+++ LogiHub.Web/Areas/Inventari/Sessioni/RilevamentoUbicazioneViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using LogiHub.Services.Inventari.Sessioni.DTO;
+using LogiHub.Web.Features.SearchCard;
+using LogiHub.Web.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LogiHub.Web.Areas.Inventari.Sessioni;
+
+public class RilevamentoUbicazioneViewModel : PagingViewModel
+{
+    public Guid SessioneId { get; set; }
+    public Guid UbicazioneId { get; set; }
+    public string NomeUbicazione { get; set; }
+
+    public SearchCardViewModel SearchCard { get; set; }
+    public IEnumerable<PezzoInventarioDTO> Pezzi { get; set; }
+
+    public override IActionResult GetRoute()
+    {
+        return MVC.Inventari.Sessioni
+            .RilevamentoUbicazione(
+                SessioneId,
+                UbicazioneId,
+                SearchCard.Query,
+                Page,
+                PageSize
+            )
+            .GetAwaiter()
+            .GetResult();
+    }
+}
